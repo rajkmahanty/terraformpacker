6e14c1b018fb5548719bb0ba7868f011e89171b4
Ubuntu <devops@TerraformPackerVM.rmuolrt33htevbkkibxeznfwta.ix.internal.cloudapp.net>
2018-10-03 11:42:18 +0000

    Packer Template change 7

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,14 @@
-###### AZURE section start
+##### AZURE section start
 
-variable "client_id" {}
-variable "client_secret" {}
-variable "tenant_id" {}
-variable "subscription_id" {}
+#variable "client_id" {ca9d7487-c484-49a2-b7a0-4b0a4cf12d9b}
+#variable "client_secret" {bcc0f396-5a5e-4231-a951-c7e8677f7933}
+#variable "tenant_id" {2c1f8453-5cc2-44f7-a93f-49ab55dcaab8}
+#variable "subscription_id" {3216b735-086e-46b2-9363-44095f8213e1}
+
+variable "client_id" { default = "ca9d7487-c484-49a2-b7a0-4b0a4cf12d9b" }
+variable "client_secret" { default = "bcc0f396-5a5e-4231-a951-c7e8677f7933" }
+variable "tenant_id" { default = "2c1f8453-5cc2-44f7-a93f-49ab55dcaab8" }
+variable "subscription_id" { default = "3216b735-086e-46b2-9363-44095f8213e1" }
 
 variable "computer_name" {
   description = "Machine_name"
@@ -12,20 +17,22 @@ variable "computer_name" {
 
 variable "admin_username" {
   description = "Machine_user_name"
-  default = "please input your value"
+  default = "devops"
 }
 
 variable "admin_password" {
   description = "Machine_password"
-  default = "please inpuut your vaule"
+  default = "welcome1"
 }
 
 variable "resource_group" {
   description = "Resource Group"
+  default = "abc-sg"
 }
 
 variable "vm_size" {
   description = "Virtual Machine size"
+  default = "Standard_D2s_v3"
 }
 
 ###### AZURE section end
