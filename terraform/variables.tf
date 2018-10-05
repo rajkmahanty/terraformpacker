e56ea61cf962fb696fcee240e76a697f021ae152
Ubuntu <devops@TerraformPackerVM.rmuolrt33htevbkkibxeznfwta.ix.internal.cloudapp.net>
2018-10-05 05:23:06 +0000

    Azure App Changes

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,8 +5,8 @@
 #variable "tenant_id" {2c1f8453-5cc2-44f7-a93f-49ab55dcaab8}
 #variable "subscription_id" {3216b735-086e-46b2-9363-44095f8213e1}
 
-variable "client_id" { default = "6a88c230-e93c-4de1-899c-261632a20f2e" }
-variable "client_secret" { default = "d2ecb64f-4198-457c-a302-a442fe759fcd" }
+variable "client_id" { default = "6ba8dafc-eadd-4388-a4c3-0fdcb564a950" }
+variable "client_secret" { default = "6ba8dafc-eadd-4388-a4c3-0fdcb564a950" }
 variable "tenant_id" { default = "027e173a-9063-4df7-9aea-b38a90e8db3a" }
 variable "subscription_id" { default = "5e4c5a35-8adf-4cb9-89f8-6ab8f53068e0" }
 
